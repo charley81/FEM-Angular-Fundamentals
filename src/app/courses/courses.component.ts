b4fbe60d355a083aa4b9ec4436ebb0f11a2405ca
ChrisKnowsDev <chrisharley81@gmail.com>
2023-06-23 16:14:57 -0400

    course displaying in course list section and showing json of selected course

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent implements OnInit {
+  // 1. render courses in a list
+  // 2. select a course
+  // 3. render selected course
+  courses = [
+    {
+      id: 1,
+      title: 'Angular 13 Fundamentals',
+      description: 'Learn the fundamentals of Angular 13',
+      percentComplete: 26,
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'GIT Fundamentals',
+      description: 'Learn the fundamentals GIT',
+      percentComplete: 16,
+      favorite: false,
+    },
+    {
+      id: 3,
+      title: 'Typescript Fundamentals',
+      description: 'Learn the fundamentals of Typescript',
+      percentComplete: 22,
+      favorite: true,
+    },
+    {
+      id: 4,
+      title: 'Testing Fundamentals',
+      description: 'Learn the fundamentals of Testing',
+      percentComplete: 66,
+      favorite: false,
+    },
+  ];
+  selectedCourse = null;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+}
